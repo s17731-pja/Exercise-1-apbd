d2b80cee0dd0a2ac2ec06ba6782a27ef26d3bb98
s17731 <s17731@pjwstk.edu.pl>
2020-02-29 19:38:22 +0100

    new console project app

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+            }
+
+            Console.WriteLine("End!");
+        }
+    }
+}
